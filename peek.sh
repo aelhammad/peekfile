65d159172c9d9770eb6a2b8e332be3be0cf832f3
Allal El Hommad <allalelhommad@mar-101-100.eduroamcm.upf.edu>
2023-11-02 09:20:23 +0100

    improved version

--- peek.sh
+++ peek.sh
@@ -1,6 +1,16 @@
 #!/bin/zsh
+#!/bin/zsh
+
+# Check if the second argument (number of lines) is provided
+if [[ -z $2 ]]; then
+  lines=3  # Default to 3 lines if not provided
+else
+  lines=$2
+fi
+
+head -n $lines $1
+echo "..."
+tail -n $lines $1
 
 
-head -n $2 $1
-echo ...
-tail -n $2 $1
+
